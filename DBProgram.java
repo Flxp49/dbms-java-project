bc689067e50df54e5c457b097893ea6a64f20b1d
flxp49 <emxdahmad@gmail.com>
2022-12-25 21:42:43 +0530

    connected to db
    Co-authored-by: Haleema Sadia <haleemasadia0@users.noreply.github.com>

--- DBProgram.java
+++ DBProgram.java
@@ -1,4 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DBProgram {
     public static void main(String[] args) {
+        try {
+            // env variable
+            Dotenv dotenv = Dotenv.load();
+
+            // Registering the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Connection
+            Connection conn = DriverManager.getConnection(dotenv.get("mysqlurl"));
+
+            // Query statement object
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery("show databases ");
+            System.out.println("List of databases: ");
+            while (rs.next()) {
+                System.out.print(rs.getString(1));
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
--- databaseDesign.sql
+++ databaseDesign.sql
@@ -1,17 +1,75 @@
-show databases;
 create database fclub;
 use fclub;
+
+-- Players table 
 create table players (
 	pid int primary key,
-    name varchar(30) not null,
+    p_name varchar(30) not null,
     dob date not null,
-	pos varchar(30) not null,
+	pos varchar(5) not null,
     wage int not null,
-    age int not null,
     cont_start date not null,
     cont_end date not null,
+    tid int,
     CONSTRAINT input_pos CHECK (pos IN ('ATT', 'MID', 'DEF', 'GK'))
     );
-insert into players values(1,"Hamza", "2000-12-01", "ATT", 45000, 20, "2000-12-01", "2000-12-01");
-select * from players;
-    
+
+-- Personal Trainers table
+create table personal_trainers (
+    ptid int primary key,
+    email varchar(30),
+    pt_name varchar(30),
+    address varchar(30),
+    contact_no varchar(15),
+    pid int,
+    foreign key(pid) references players(pid)
+    );  
+
+-- Coaches table
+create table coaches (  
+    cid int primary key,
+    c_name varchar(30),
+    contact_no varchar(15),
+    address varchar(30),
+    coaching_pos varchar(5),
+    email varchar(30),
+    pid int, 
+    foreign key(pid) references players(pid)
+    );
+
+-- Training Sessions table
+create table training_sessions(
+    tid int primary key,
+    cid int,
+    start_time time,
+    end_time time,
+    info varchar(100),
+    foreign key(cid) references coaches(cid)
+    );
+
+-- Nutritionist table
+create table nutritionist(
+    nid int primary key,
+    n_name varchar(30),
+    address varchar(20),
+    email varchar(30),
+    contact_no varchar(15),
+    pid int,
+    foreign key (pid) references players(pid)
+    );
+
+-- Doctors Training
+create table doctors(
+    did int primary key,
+    d_name varchar(30),
+    shift varchar(10),
+    email varchar(30),
+    pid int,
+    contact_no varchar(15),
+    address varchar(20),
+    foreign key (pid) references players(pid),
+    CONSTRAINT input_shift CHECK (shift IN ('Morning', 'Evening'))
+    );
+
+-- Adding foreign key 'TID' to players table
+alter table players add foreign key(tid) references training_sessions(tid);
